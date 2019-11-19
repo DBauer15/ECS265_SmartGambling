9d3f34b84445c2cef74386e3bbd93e19ce1c5e6d
Lukas Maximilian Masopust <lukasmasopust@Lukas-MacBook-Pro.local>
2019-11-19 00:38:30 -0800

    Added basic roulette board

---
+++ client/src/components/BoardCell.vue
@@ -0,0 +1,63 @@
+<template>
+<div>
+  <div class="board-cell" :class="getCellColorClass()">
+      {{config.number}}
+  </div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'BoardCell',
+  props: {
+    config: Object
+  },
+  methods: {
+    getCellColorClass() {
+        return 'board-cell-' + this.config.type;
+    }
+  },
+}
+</script>
+
+<style>
+    .board-cell {
+        border: 0.02em solid black;
+        height: 2em;
+        width: 2em;
+        color: white;
+        font-size: 1.5em;
+        font-family: serif;
+        font-weight: bold;
+        line-height: 2em;    
+    }
+    .board-cell:hover {
+        cursor: pointer;
+        opacity: 0.7;
+    }
+    .board-cell-red {
+        background-color: red
+    }
+    .board-cell-black {
+        background-color: black
+    }
+    .board-cell-green {
+        background-color: green;
+        height: 6.24em;
+        line-height: 6.24em;
+    }
+    .board-cell-split-h {
+        background-color: gray;
+        height: 0.4em;
+    }
+    .board-cell-split-v {
+        background-color: gray;
+        width: 0.4em;
+    }
+    .board-cell-corner {
+        background-color: #ccc;
+        height: 0.4em;
+        width: 0.4em;
+    }
+</style>
---
+++ client/src/components/RouletteBoard.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <!--
+        <div style="float: left">
+            <BoardCell :config="{
+                number: 0,
+                color: colors[2]
+            }"></BoardCell>
+        </div>
+        -->
+        <div>
+            <div v-for="i in 3" style="float: left">
+                <div v-for="j in 12" style="float: left">
+                    <BoardCell style="float:left" :config="{
+                        number: board_numbers[i-1][j-1],
+                        type: colors[board_colors[i-1][j-1]]
+                    }"></BoardCell>
+                    <BoardCell style="float:left" v-if="j != 12" :config="{
+                        number: null,
+                        type: 'split-v'
+                    }"></BoardCell>
+                    <div class="clearfix"></div>
+                    <BoardCell style="float:left" v-if="i != 3" :config="{
+                        number: null,
+                        type: 'split-h'
+                    }"></BoardCell>
+                    <BoardCell style="float:left" v-if="j != 12 && i != 3" :config="{
+                        number: null,
+                        type: 'corner'
+                    }"></BoardCell>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <!--
+        <div style="float: left">
+            <BoardCell :config="{
+                number: 0,
+                color: colors[3]
+            }">
+            </BoardCell>
+        </div>
+        -->
+    </div>
+</template>
+
+<script>
+import BoardCell from './BoardCell.vue'
+
+export default {
+    components: {
+        BoardCell
+    },
+    data() {
+
+        return {
+            colors: ['red', 'black', 'green', 'gray', 'white'],
+            board_colors: [
+                [0,1,0,0,1,0,0,1,0,0,1,0],
+                [1,0,1,1,0,1,1,0,1,1,0,1],
+                [0,1,0,1,1,0,0,1,0,1,1,0]
+            ],
+            board_numbers: [
+                [3,6,9,12,15,18,21,24,27,30,33,36],
+                [2,5,8,11,14,17,20,23,26,29,32,35],
+                [1,4,7,10,13,16,19,22,25,28,31,34]
+            ],
+        }
+    }
+}
+</script>
+
+<style>
+</style>
